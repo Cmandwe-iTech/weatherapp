68e0fc7358015d9bae41843f7157f6fbacdc4a74
Chhatrapati Mandwe <mandwec12@gmail.com>
2023-02-27 20:21:50 +0530

    add

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -18,19 +18,19 @@ const Weather = () => {
       });
   }, [city]);
   return (
-    <div className="container">
-      <div>
+    <div className="container" style={{textAlign:"center"}}>
+      <div style={{margin:"50px auto 0"}}>
         <input
           type="text"
           placeholder="search for city..."
           onChange={(e) => {setCity(e.target.value)}}
         />
       </div>
-      <div>
+      <div style={{height:"300px",width:"300px",border:"2px solid red",margin:"20px auto 0"}}>
         {
             !city? <div><p>please enter valid city</p></div>: weather.map((item, i) => {
                 return (
-                  <div>
+                  <div key={i}>
                     <p>Weather Details of City: {item.name}</p>
                     <p>Current Temperature: {item.cod - 170} &#8451;</p>
                     <p>Temperature Range: {item.cod - 180} &#8451; to {item.cod - 170} &#8451;</p>
